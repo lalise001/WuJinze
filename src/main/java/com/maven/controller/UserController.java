0002f4c4a344b3a20002c23eb64472b66892ca8d
lalise001 <852587083@qq.com>
2020-04-30 16:14:56 +0800

    lalise再次修改文件

--- src/main/java/com/maven/controller/UserController.java
+++ src/main/java/com/maven/controller/UserController.java
@@ -5,6 +5,6 @@ public class UserController {
 	public void say() {
 		System.out.print("aaaaaaaa");
 		System.out.print("cccccccc");
-		System.out.print("demo1");
+		System.out.print("demo");
 	}
 }
